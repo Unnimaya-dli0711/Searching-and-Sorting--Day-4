dde25d97116389f5a9fae93a13fa30418d40fd47
Unnimaya T <Unnimaya.T@godigit.com>
2024-07-28 23:21:04 +0530

    duplicates

---
+++ Arraylists/Duplicates.java
@@ -0,0 +1,27 @@
+package Arraylists;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Duplicates {
+    public static void main(String[] args){
+        Scanner scannerobject=new Scanner(System.in);
+        System.out.println("Enter number of elements in array");
+        int number=scannerobject.nextInt();
+        ArrayList<Integer> array=new ArrayList<>();
+        ArrayList<Integer> duplicates=new ArrayList<>();
+        System.out.println("Enter the elements");
+        for(int i=0;i<number;i++){
+            array.add(scannerobject.nextInt());
+        }
+        for(int i=0;i<array.size();i++){ 
+                int element=array.get(i);
+                array.remove(i);
+                if(array.contains(element)){
+                    duplicates.add(element);
+                    System.out.println(element);
+                }
+
+        }
+    }
+}
